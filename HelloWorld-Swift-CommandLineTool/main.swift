3489c0852349fcd05468c5c95a10be5cf122c39d
Josep B <ballber@gmail.com>
2018-10-11 02:21:42 +0200

    Initial Commit

---
+++ HelloWorld-Swift-CommandLineTool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld-Swift-CommandLineTool
+//
+//  Created by Joe on 11/10/18.
+//  Copyright © 2018 UOC. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
